fe56bad19ac1e53224349b9659719ed0c3337ddc
aottolini <aottolini@agro.uba.ar>
2021-12-11 17:25:17 -0300

    Day07-part2

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -4,11 +4,32 @@ fun positions(input: String): List<Int> {
     return input.split(",").map(String::toInt)
 }
 
-fun median(positions: List<Int>): Int {
-    return positions.sorted()[positions.size / 2]
+fun positionsCount(positions: List<Int>): Map<Int, Int> {
+    return positions.groupingBy { it }.eachCount()
 }
 
-fun fuelSpent(positions: List<Int>): Int {
-    val median = median(positions)
-    return positions.sumOf { abs(it - median) }
+fun bestPositionFuelCost(positions: List<Int>, fuelCost: (Int, Int) -> Int): Int? {
+    val possiblePositions = possiblePositions(positions)
+    val count = positionsCount(positions)
+    val fuelCosts = possiblePositions.map { position -> totalFuelCost(count, position, fuelCost) }
+    return fuelCosts.minOrNull()
+}
+
+fun totalFuelCost(positionsCount: Map<Int, Int>, position: Int, fuelCost: (Int, Int) -> Int): Int {
+    return positionsCount.map { fuelCost(it.key, position) * it.value }.sum()
+}
+
+fun possiblePositions(positions: List<Int>): IntRange {
+    val sortedPositions = positions.sorted()
+    val min = sortedPositions.first()
+    val max = sortedPositions.last()
+    return min..max
+}
+
+fun constantFuelCost(from: Int, to: Int): Int {
+    return abs(from - to)
+}
+
+fun incrementalFuelCost(from: Int, to: Int): Int {
+    return (1..abs(from - to)).sum()
 }
--- src/test/kotlin/Day07.kt
+++ src/test/kotlin/Day07.kt
@@ -9,8 +9,15 @@ internal class Day07 {
     @Test
     fun part1() {
         val positions = positions(input)
-        val fuelSpent = fuelSpent(positions)
-        assertEquals(37, fuelSpent)
+        val fuelCost = bestPositionFuelCost(positions, ::constantFuelCost)
+        assertEquals(37, fuelCost)
+    }
+
+    @Test
+    fun part2() {
+        val positions = positions(input)
+        val fuelCost = bestPositionFuelCost(positions, ::incrementalFuelCost)
+        assertEquals(168, fuelCost)
     }
 
 }
